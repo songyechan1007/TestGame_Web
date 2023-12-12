3bb4546bc462043261b25afe01788fddcf459910
songyechan1007 <songyechan1007@gmail.com>
2023-12-12 11:29:01 +0900

    testgame init

---
+++ src/main/java/com/testgame/testgameweb/TestGameWebApplication.java
@@ -0,0 +1,13 @@
+package com.testgame.testgameweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGameWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGameWebApplication.class, args);
+	}
+
+}
